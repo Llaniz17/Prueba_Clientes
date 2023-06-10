be1349cd07d76754d6a383bfa54d84d787386330
Llaniz17 <llanessuarez17@gmail.com>
2023-06-10 12:42:57 -0500

    subir api

--- cliente_api/settings.py
+++ cliente_api/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,8 +84,12 @@ WSGI_APPLICATION = "cliente_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "TTKLTZHGqitk48FQInzv",
+        "HOST": "containers-us-west-167.railway.app",
+        "PORT": "6044",
     }
 }
 
--- clients/models.py
+++ clients/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Client(models.Model):
     name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
-    document_number = models.CharField(max_length=50)
+    document_number = models.CharField(max_length=51)
     birthdate = models.DateField()
     created_date = models.DateField(auto_now_add=True)
 
